5f1402125ad23986ed2bf10f376aea8c3a41d17f
James Carman <jcarman@apache.org>
2015-01-06 16:19:48 -0500

    Putting together some ideas.

--- src/main/java/org/beaucoup4j/elle/StringLambdas.java
+++ src/main/java/org/beaucoup4j/elle/StringLambdas.java
@@ -1,15 +1,31 @@
 package org.beaucoup4j.elle;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class StringLambdas {
-
-    public static Function<String, String> reverse() {
-        return (s) -> new StringBuilder(s).reverse().toString();
-    }
+//----------------------------------------------------------------------------------------------------------------------
+// Static Methods
+//----------------------------------------------------------------------------------------------------------------------
 
     public static Supplier<String> empty() {
         return () -> "";
+    }
+
+    public static Predicate<String> isEmpty() {
+        return (s) -> s == null || s.isEmpty();
+    }
+
+    public static Function<String, String> reverse() {
+        return (s) -> s == null ? null : new StringBuilder(s).reverse().toString();
+    }
+
+//----------------------------------------------------------------------------------------------------------------------
+// Constructors
+//----------------------------------------------------------------------------------------------------------------------
+
+    private StringLambdas() {
+
     }
 }
